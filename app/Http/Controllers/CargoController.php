9ef6be23d719c3663b0b6da1971869860857fb30
Juan Fajardo <juanfajardo@potosi.bo>
2017-05-02 16:47:21 -0400

    CargoControlador

--- app/Http/Controllers/CargoController.php
+++ app/Http/Controllers/CargoController.php
@@ -27,7 +27,7 @@ class CargoController extends Controller
 
   public function store(Request $request){
     $request['user_id'] = 1;
-    return  $request->all();
+    //return  $request->all();
     $dato = new Cargo;
     $dato->fill( $request->all() );
     $dato->save();
