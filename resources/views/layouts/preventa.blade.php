fde84b65ce0ce24d8e9312f264b2b6102b9a44ac
Juan Fajardo <juanfajardo@potosi.bo>
2017-06-13 11:38:11 -0400

    Pre Venta

--- resources/views/layouts/preventa.blade.php
+++ resources/views/layouts/preventa.blade.php
@@ -3,14 +3,19 @@
   <head>
     <meta charset="utf-8">
     <title>@yield('title')</title>
-    <link href="{{asset('assets/css/materialize.min.css')}}" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css" rel="stylesheet">
 
     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   </head>
-  <body>
+  <body ng-app="AdycttoBett0">
     <div class="container">
 
-        @yield('cuerpo')
+      <div class="row">
+          <div class="col-lg-12">
+              @yield('cuerpo')
+          </div>
+      </div>
+
     </div>
 
     <script src="{{asset('assets/js/jquery.js')}}"></script>
@@ -20,8 +25,6 @@
     <script src="{{asset('assets/angular/angular-resource.js')}}"></script>
     <script src="{{asset('assets/angular/angular-route.js')}}"></script>
     <script src="{{asset('assets/angular/angular-animate.js')}}"></script>
-    <script src="{{asset('assets/angular-datatables/angular-datatables.min.js')}}"></script>
-    <script src="{{asset('assets/angular-datatables/jquery.dataTables.min.js')}}"></script>
     @yield('js')
   </body>
 </html>
--- resources/views/preventa/index.blade.php
+++ resources/views/preventa/index.blade.php
@@ -1,24 +1,21 @@
 @extends('layouts.preventa')
 
 @section('title')
-	preventa
+	Pre Venta
 @endsection
 
 @section('titulo')
 Pre-Venta FEIPOBOL
 @endsection
 
-
 @section('cuerpo')
 <div ng-view></div>
 @endsection
 
-
 @section('js')
 <script type="text/javascript">
 'use strict';
-///Rutas de Angular para Dosificaciones
-angular.module('AdycttoBett0', ['ngResource', 'ngRoute', 'ngAnimate', 'datatables'])
+angular.module('AdycttoBett0', ['ngResource', 'ngRoute', 'ngAnimate'])
 .config(function($routeProvider){
         $routeProvider
         .when('/crear', {
@@ -26,12 +23,12 @@ angular.module('AdycttoBett0', ['ngResource', 'ngRoute', 'ngAnimate', 'datatable
           controller: 'CrearCtrl'
         })
 });
-///Servicio de Angular para Dosificaciones
+
 var va = angular.module('AdycttoBett0');
 va.factory('PreventaRecursos', function($resource){
   return $resource('../index.php/registro/:id', { id:"@id"}, { update: { method: "PUT" } } );
 })
-///Controladores de Angular para Dosificaciones
+
 .controller('CrearCtrl', ['$scope', '$http', 'PreventaRecursos', '$location', '$timeout', function($scope, $http, PreventaRecursos, $location, $timeout){
   $scope.titulo = 'Preventa';
   $scope.boton = "Reservar";
@@ -41,27 +38,9 @@ va.factory('PreventaRecursos', function($resource){
 	$scope.mostrar = "SI";
 	var base64="";
 
-
-	$(document).ready(function(){
-	$("#imagen").change(function(){
-			var ArchivoSeleccionado = document.getElementById("imagen").files;
-			if(ArchivoSeleccionado.length > 0){
-				var FileToLoad = ArchivoSeleccionado[0];
-				var fileReader = new FileReader();
-				fileReader.onload = function(fileLoadedEvent){
-					base64 = fileLoadedEvent.target.result;
-					$scope.img = base64;
-					//document.getElementById("preview").setAttribute("src",base64);
-					//$("#preview").show();
-				};
-				fileReader.readAsDataURL(FileToLoad);
-			}
-		});
-
-
+	/*
   $scope.guardarPreventa = function(){
-		$scope.Preventa.imagen = $scope.img;
-    StandRecursos.save($scope.Preventa, function(data){
+    PreventaRecursos.save($scope.Preventa, function(data){
           var respuesta = data['respuesta'];
           if(respuesta == '200_OK'){
             $scope.panel = "alert alert-info";
@@ -75,8 +54,9 @@ va.factory('PreventaRecursos', function($resource){
           }
     });
   };
-}]);
+	*/
 
+}]);
 </script>
 
 @endsection
