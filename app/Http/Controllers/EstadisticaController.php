f6268e0c428cdddff49963adb4b2a3946c41ae58
Juan Fajardo <juanfajardo@potosi.bo>
2017-06-20 05:48:49 -0400

    nuevo repetitivos

---
+++ app/Http/Controllers/EstadisticaController.php
@@ -0,0 +1,12 @@
+
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EstadisticaController extends Controller
+{
+    public function angular(){
+      
+    }
+}
---
+++ app/Repetitivo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Repetitivo extends Model
+{
+  protected $table    = 'repetitivos';
+  protected $fillable = ['id', 'fecha', 'hora', 'categoria', 'sexo', 'marcado', 'persona_id', 'horario_id', 'user_id'];
+}
