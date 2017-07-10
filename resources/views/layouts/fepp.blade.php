c064b329fbdf342de9db5871e9c0b4fa8d0fb8dc
Juan Fajardo <juanfajardo@potosi.bo>
2017-07-10 13:47:45 -0400

    Login Terminate

--- resources/views/layouts/fepp.blade.php
+++ resources/views/layouts/fepp.blade.php
@@ -48,7 +48,7 @@
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> {{ \Auth::user()->name }} <b class="caret"></b></a>
                     <ul class="dropdown-menu">
-                        <li><a href="#"><i class="fa fa-fw fa-user"></i> Datos Personales</a></li>
+                        <li><a href="{{asset('index.php/usuarios/info/ver')}}"><i class="fa fa-fw fa-user"></i> Datos Personales</a></li>
                         <li><a href="{{asset('index.php/logout')}}"><i class="fa fa-fw fa-power-off"></i> Cerrar Sesion</a></li>
                     </ul>
                 </li>
