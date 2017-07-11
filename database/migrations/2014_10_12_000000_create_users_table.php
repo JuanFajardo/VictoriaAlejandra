7d55991a7bbda179567dfb61531d2e30992bf429
Juan Fajardo <juanfajardo@potosi.bo>
2017-07-11 13:57:46 -0400

    Estadistica Segundo

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,11 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
@@ -20,17 +16,12 @@ class CreateUsersTable extends Migration
             $table->string('email');
             $table->string('password');
             $table->string('grupo');
-            $table->intenger('estado')->default(1);
+            $table->integer('estado');
             $table->rememberToken();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('users');
